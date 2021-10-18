ad943a49a5021ef2757e53f93d30111fb867aab4
Mateusz Filar <mateusz.filar@miquido.com>
2021-10-18 21:27:54 +0200

    feat: header and footer enter animation

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,10 +1,24 @@
-import { Component, ChangeDetectionStrategy, EventEmitter, Output } from '@angular/core';
+import { animate, style, transition, trigger } from '@angular/animations';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Output,
+} from '@angular/core';
+
+const enter = trigger('enter', [
+  transition('void => *', [
+    style({ opacity: 0, transform: 'translateY(120%)' }),
+    animate('0.25s 0.6s ease-out', style({ opacity: 1, transform: 'none' })),
+  ]),
+]);
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  animations: [enter],
 })
 export class FooterComponent {
   @Output() showRules = new EventEmitter<void>();
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,26 @@
+import {
+  animate,
+  query,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { AppService } from '../../app.service';
+
+const enter = trigger('enter', [
+  transition('void => *', [
+    style({ opacity: 0, transform: 'translateY(-120%)' }),
+    animate('0.25s 0.4s ease-out', style({ opacity: 1, transform: 'none' })),
+  ]),
+]);
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  animations: [enter],
 })
 export class HeaderComponent {
   score$ = this.appService.getScore();
